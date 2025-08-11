1de9a82c7f626c00e403c6e1874ab5eaea720899
livin <livin@kayama>
2025-08-11 09:14:38 +0900

    環境編集

--- src/main/java/com/example/gundamsystem/repository/EmployeeRepository.java
+++ src/main/java/com/example/gundamsystem/repository/EmployeeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.gundamsystem.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
     
     // 社員IDで検索（ログイン時などに使用）
     Employee findByEmployeeId(String employeeId);
